802ad269ce8ad3fd75b973faaab7303a0f2a60de
raghavd17 <raghav.d17@gmail.com>
2015-05-08 11:32:19 +0530

    using item.push

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,43 @@
 $(document).ready(function() {
 	$.getJSON( "json/user.json", function( data ) {
-	  //optional stuff to do after success
-	  var foodItems = '<ul class="items">';
+	var items = [];
+		$.each( data.user, function( i, item ) {
 
-		for(var i = 0; i < data.user.length; i++ ) {
-			
-			var itemId = data.user[i].id;
-			var itemName = data.user[i].name;
-			var itemPlace = data.user[i].place;
-			var itemUserProfile = data.user[i].user_image;
-			var itemFoodImg = data.user[i].food_image;
-			var itemDesc = data.user[i].description;
+			var itemId = item.id;
+			var itemName = item.name;
+			var itemPlace = item.place;
+			var itemUserProfile = item.user_image;
+			var itemFoodImg = item.food_image;
+			var itemDesc = item.description;
+
+	items.push( '<li><div class="food_item" id="food_item_'+ itemId+'"> '
+				+'<div class="image" id='+itemId+'> <ul class="user_review"><li> 101 Likes</li> <li> 50 Discuss</li></ul>'
+				+'<img src='+itemFoodImg+' alt=' + itemName + ' /> </div> <div class="author clearfix"> '
+				+'<img src='+itemUserProfile+'  alt="profile" class="profile" />'
+				+'<h1 class="title"> '+itemName+' <cite> '+ itemPlace +' </cite></h1></div><div class="description"><p>'+itemDesc+'</p></div></div></li>' );
+	});
+
+	$( "<ul/>", {
+		"class": "items",
+		html: items.join( "" )
+	}).appendTo( "#main_container" );
+
+// for (var i = 0; i < items.length; i++) {
+// 	console.log( "Items of " + i + " :\n" + items[i]);
+// };
+
+	$('#main_container').append('<div class="product_info"></div>');
 
 
-			foodItems += '<li><div class="food_item" id="food_item_'+itemId+'"><div class="image" id="'+itemId+'"><ul class="user_review"><li> 101 Likes</li> <li> 50 Discuss</li></ul>'
-					+'<img src=" '+itemFoodImg+' " alt=" '+ itemName +' " /> </div>'
-					+'<div class="author clearfix"> <img src=" '+itemUserProfile+' " alt="profile" class="profile" />'
-					+'<h1 class="title"> '+itemName+' <cite> '+itemPlace +' </cite></h1></div>' 
-					+'<div class="description"><p>'+itemDesc+'</p></div></div></li>';			
-
-			// console.log("Id: " + itemId + "\n Name: " + itemName + "\n Place:" +itemPlace + "\n User Profile" +itemUserProfile+ "\n Food Img" + itemFoodImg + "\n Item description" + itemDesc);
-			// console.log (foodItems);
-		}
-		foodItems+='</ul>';
-		$('#main_container').html(foodItems);
-		$('#main_container').append('<div class="product_info"></div>');
-
-
-		$('div.image').on('click',  function(event) {
+		$('div.image').on('click',  function() {
 			var itemId = $(this).attr("id");
 				itemId = "food_item_"+itemId;
 			var positionLft = $('.product_info').width();
-
+			// console.log('after click image');
 			$('.product_info').html('<div class="arrow_close"><img src="img/arrow_close.png" alt="Close"></div>'); 
+			// console.log('after html arrow');
 			$('#'+itemId).clone().appendTo('.product_info');
-
+			// console.log('after clone appendTo');
 			$('.product_info').append('<form  method="get" accept-charset="utf-8">'
 							+'<input type="text" name="" value="" placeholder="Name">'
 							+'<input type="email" name="" value="" placeholder="Email Address">'
@@ -42,33 +45,20 @@ $(document).ready(function() {
 							+'<input type="button" name="" value="Submit" class="btn btn-primary">'
 							+' <input type="Reset" name="" value="Cancel" class="btn btn-inverse"></form>');
 				
-			
+			// console.log('after form append');
 			$('ul.items').css('left', - (positionLft + 20));
 			// alert('width' + positionLft);
 			$('.product_info').css('right','0');
 			
 
-			$('.arrow_close').on('click',  function(event) {
+			$('.arrow_close').on('click',  function() {
 				$('ul.items').removeAttr('style');
 				// alert('width' + positionLft);
 				$('.product_info').removeAttr('style');
-				/* Act on the event */
+
 			});
-			/* Act on the event */
 
 		});
-
-
-	
-
-
-
-
-	});
-
-
-	
-	// alert('hello');
-	
+});
 	
 });
